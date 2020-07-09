84f27aef1eba2319ced419e077aa5d3ce8e94138
VicHofs <57238498+VicHofs@users.noreply.github.com>
2020-07-09 13:24:35 -0300

    fix missing files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/logo.png';
 import './App.css';
 import api from './services/api';
 
 function App() {
+	const [questions, setQuestions] = useState([{correct_answer: "loading", incorrect_answers: ["loading", "loading", "loading"], question: "Loading..."}]);
 	const [gameOn, setGameOn] = useState(false);
 	const [thisQuestion, setThisQuestion] = useState();
 	const [correctAnswer, setCorrectAnswer] = useState();
-	let questions = {};
 	const [points, setPoints] = useState(0);
-	let questionNum = 0;
+  const [questionNum, setQuestionNum] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
 
 	const checkAnswer = (choice) => {
-    if (choice === correctAnswer) setPoints(points + 1);
-    nextQuestion();
+		if (choice === correctAnswer) {
+			setPoints(points + 1);
+			console.log('correct!');
+		} else console.log("uh oh, that's incorrect!");
+		setQuestionNum(questionNum+1);
 	};
 
 	const resolveAscii = (text) => {
-		for (const char of text) {
-			if (char === '&') {
-				text = text.replace('&#039;', "'");
-				text = text.replace('&ldquo;', '"');
-				text = text.replace('&rdquo;', '"');
-				text = text.replace('&oacute;', 'o');
-				text = text.replace('&quot;', '"');
-				break;
-			}
+		while (text.includes('&')) {
+			text = text.replace('&#039;', "'");
+			text = text.replace('&ldquo;', '"');
+			text = text.replace('&rdquo;', '"');
+			text = text.replace('&oacute;', 'o');
+			text = text.replace('&quot;', '"');
 		}
 		return text;
 	};
 
 	const startGame = () => {
 		api.get().then((response) => {
-			setPoints(0);
-			questionNum = 0;
-			questions = response.data.results;
-			console.log(questions);
-			nextQuestion();
+			setQuestions(response.data.results);
+			console.log(response.data.results);
+			setQuestionNum(0);
+      setPoints(0);
+      setGameOver(false);
 			setGameOn(true);
 		});
 	};
@@ -52,16 +53,19 @@ function App() {
 		return a;
 	};
 
-	const nextQuestion = () => {
-		let newQuestion = questions[questionNum];
-		setCorrectAnswer(newQuestion.correct_answer);
-		let allChoices = newQuestion.incorrect_answers;
-		allChoices = shuffle([...allChoices, newQuestion.correct_answer]);
-		setThisQuestion(
-			[...allChoices, newQuestion.question].map(resolveAscii)
-		);
-		questionNum++;
-	};
+	useEffect(() => {
+    if ( 24 > questionNum && questionNum >= 0) {
+      let newQuestion = questions[questionNum];
+      setCorrectAnswer(newQuestion.correct_answer);
+      let allChoices = newQuestion.incorrect_answers;
+      allChoices = shuffle([...allChoices, newQuestion.correct_answer]);
+      setThisQuestion([...allChoices, newQuestion.question].map(resolveAscii));
+    }
+    else {
+      setGameOn(false);
+      setGameOver(true);
+    }
+	}, [questionNum, questions]);
 
 	return (
 		<div className="main">
@@ -104,6 +108,13 @@ function App() {
 					</button>
 				</ul>
 			)}
+      { gameOver && (
+        <div className="main">
+          <h2>Your Score:</h2>
+          <h2>{points}</h2>
+          <button className="startButton" onClick={startGame}>Play Again</button>
+        </div>
+      ) }
 		</div>
 	);
 }
